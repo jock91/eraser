cbce7dc07cfc41362a930a1ec8f4c6441dfb50aa
Takushi60130 <takushi@me.com>
2015-02-20 14:12:55 +0100

    Ajout de moment.js

--- src/Erazr/Bundle/SiteBundle/Resources/public/sources/js/script.js
+++ src/Erazr/Bundle/SiteBundle/Resources/public/sources/js/script.js
@@ -12,33 +12,11 @@ function init(){
 }
 
 function resize(){
-	fullHeight('aside','min');
 }
 
 /* ------------------------ */
 
-function fullHeight(e,min){
-	var winH = $(window).height();
-	if(min){
-		$(e).css('min-height',winH);
-	}else{
-		$(e).css('height',winH);
-	}
-}
-
 function hour(){
-/*
- *
- * Récupérer heure PHP
- *
- *
- */
-	var date = new Date(),
-	h = date.getHours(),
-	m = date.getMinutes(),
-	s = date.getSeconds();
-	if(h<10){h="0"+h}
-	if(m<10){m="0"+m}
-	if(s<10){s="0"+s}
-	$('.sideTimer').html(h+':'+m+':'+s);
+	var date = moment().format('H:mm:ss');
+	$('.sideTimer').html(date);
 }
