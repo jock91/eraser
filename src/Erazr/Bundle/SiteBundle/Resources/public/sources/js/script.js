a5a37e5a8d09958b4bad61b84a36743160c6625f
Takushi60130 <takushi@me.com>
2015-02-25 15:10:06 +0100

    Inté

--- src/Erazr/Bundle/SiteBundle/Resources/public/sources/js/script.js
+++ src/Erazr/Bundle/SiteBundle/Resources/public/sources/js/script.js
@@ -9,6 +9,19 @@ function init(){
 	resize();
 	hour();
 	setInterval(hour,1000);
+	$('[data-toggle="tooltip"]').tooltip();
+	$('[data-countdown]').each(function(){
+		var el = $(this),
+			finalDate = $(this).data('countdown');
+		$(el).countdown(finalDate,function(event){
+			var days = event.strftime('%-D'),
+				hours = event.strftime('%H');
+			if(days != 0){var days = '24';}
+			var hours = parseInt(days) + parseInt(hours);
+			if(hours < '10'){var hours = '0'+hours;}
+			$(el).html(event.strftime(hours+':%M:%S'));
+		});
+	});
 }
 
 function resize(){
