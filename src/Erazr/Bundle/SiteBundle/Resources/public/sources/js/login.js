59402c1e26b5f5d4397b6ff9dc34879f1dbe0074
Takushi60130 <takushi@me.com>
2015-02-26 12:05:39 +0100

    Connexion fb

--- src/Erazr/Bundle/SiteBundle/Resources/public/sources/js/login.js
+++ src/Erazr/Bundle/SiteBundle/Resources/public/sources/js/login.js
@@ -11,9 +11,6 @@ function init(){
 
 function resize(){
 	fullHeight('#login','min');
-	$('.btn-facebook').click(function(){
-		fb_login();
-	});
 }
 
 /* ------------------------ */
@@ -25,42 +22,4 @@ function fullHeight(e,min){
 	}else{
 		$(e).css('height',winH);
 	}
-}
-
-// Facebook connect
-window.fbAsyncInit = function(){
-	FB.init({
-		appId		: '514256792045832',
-		//channelUrl	: '//yourdomain.com/channel.html',
-		status		: true,
-		xfbml		: true
-	});
-};
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if(d.getElementById(id)){return;}
-	js = d.createElement(s);js.id = id;
-	js.src = "//connect.facebook.net/en_US/all.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-function fb_login(){
-	FB.getLoginStatus(function(response) {
-		if(response.status === 'connected') {
-			alert('Already connected, redirect to login page to create token.');
-			document.location = "{{ url("hwi_oauth_service_redirect", {service: "facebook"}) }}";
-		}else{
-			FB.login(function(response){
-				if(response.authResponse){
-					document.location = "{{ url("hwi_oauth_service_redirect", {service: "facebook"}) }}";
-				}else{
-					alert('Cancelled.');
-				}
-			},{scope: 'email'});
-		}
-	});
-}
-if(window.location.hash == '#_=_'){
-	window.location.hash = '';
-	history.pushState('',document.title,window.location.pathname);
-	e.preventDefault();
 }
