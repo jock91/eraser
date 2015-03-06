4827ffe0ec772bae79063023b86f740e153c485b
jock91 <kevindesperries@gmail.com>
2015-03-06 16:49:17 +0100

    add/remove friends

--- src/Erazr/Bundle/SiteBundle/Controller/SiteController.php
+++ src/Erazr/Bundle/SiteBundle/Controller/SiteController.php
@@ -34,7 +34,7 @@ class SiteController extends Controller
         $em = $this->getDoctrine()->getManager();
         $userFriend = $em->getRepository('ErazrUserBundle:User')->find($id);
         if( $userFriend ===  $this->getUser() ) {
-            $this->get('session')->getFlashBag()->add('error', "Florian arrête d'essayer de t'ajouter en ami !"); 
+            $this->get('session')->getFlashBag()->add('error', '<b>' . $this->getUser()."</b> arrête d'essayer de t'ajouter en ami !"); 
             return $this->redirect($request->headers->get('referer'));
         } else {
         
